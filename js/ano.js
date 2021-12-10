40558f457f4afddc46649e2c28ae7ee12a6b8f7f
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-12-10 12:58:11 -0300

    10/12 - Projeto calendário - jan_maio prontos

--- js/ano.js
+++ js/ano.js
@@ -1,5 +1,4 @@
 var ano = parseInt(prompt("Defina o ano do calendário: (Exemplo: 2020)"));
-var janeiro = document.querySelector("section.jan");
 var iniciaAno = 1;
 var diaSemana = 2;
 var numeroDias = 0;
@@ -7,7 +6,7 @@ var dia = 1;
 
 /* Verifica o dia da semana que o ano começa: */
 while (iniciaAno < ano) {
-    
+
     /* Verifica se o ano é bissexto */
     if ((iniciaAno % 4 == 0) && (iniciaAno % 100 != 0) || (iniciaAno % 400 == 0)) {
         diaSemana++;
@@ -25,7 +24,16 @@ function mudaSemana() {
     }
 }
 
+function verificaInicioMes(mes) {
+    for (var i = 0; i < numeroDias; i++) {
+        diaSemana++;
+        mudaSemana();
+    }
+}
+
 function verificaNumeroDeDiasDoMes(mes) {
+    console.log("-----------------------");
+    console.log("MÊS: " + mes);
     if ((mes == 1) || (mes == 3) || (mes == 5) || (mes == 7) || (mes == 8) || (mes == 10) || (mes == 12)) {
 
         numeroDias = 31;
@@ -49,20 +57,39 @@ function verificaNumeroDeDiasDoMes(mes) {
 function defineDiaDaSemana(mes) {
     switch (diaSemana) {
         case 0: console.log("Início do mês: Sábado");
-            criaCalendarioIniciaSab(numeroDias);
+            var diasVazio = 6;
+            criaSemana(mes);
+            criaCalendario(numeroDias, diasVazio);
             break;
+
         case 1: console.log("Início do mês: Domingo");
+            diasVazio = 0;
+            criaSemana(mes);
+            criaCalendario(numeroDias, diasVazio);
             break;
+
         case 2: console.log("Início do mês: Segunda");
+            diasVazio = 1;
             break;
+        
         case 3: console.log("Início do mês: Terça");
-            criaCalendarioTer(numeroDias);
-            break;
+            diasVazio = 2;
+            criaSemana(mes);
+            criaCalendario(numeroDias, diasVazio);
+        break;
+
         case 4: console.log("Início do mês: Quarta");
+            diasVazio = 3;
             break;
+
         case 5: console.log("Início do mês: Quinta");
+            diasVazio = 4;
             break;
+
         case 6: console.log("Início do mês: Sexta");
+            diasVazio = 5;
+            criaSemana(mes);
+            criaCalendario(numeroDias, diasVazio);
             break;
     }
     fdsDestaque(mes);
@@ -74,43 +101,52 @@ function criaSemana(mes) {
     semanas.forEach(function (semana) {
         mostraSemana("<strong class='semana'>" + semana + "</strong>");
     })
-    sdDestaque(mes);
+    sdDestaque();
 }
 
-function sdDestaque(mes) {
+function sdDestaque() {
     var semanaInteira = document.querySelectorAll(".semana");
 
-    if (mes == 1) {
-        semanaInteira.forEach(function () {
-            var sabado = semanaInteira[0];
-            var domingo = semanaInteira[6];
-            sabado.classList.add("destaque");
-            domingo.classList.add("destaque");
-        })
-    } else if (mes == 2) {
-        semanaInteira.forEach(function () {
-            var sabadoJan = semanaInteira[0];
-            var sabadoFev = semanaInteira[7];
-            var domingoJan = semanaInteira[6];
-            var domingoFev = semanaInteira[13];
-            sabadoJan.classList.add("destaque");
-            sabadoFev.classList.add("destaque");
-            domingoJan.classList.add("destaque");
-            domingoFev.classList.add("destaque");
-        })
+    for (var i = 0; i < semanaInteira.length; i++) {
+        if ((i % 7 == 0) || (i % 7 == 6)) {
+            semanaInteira[i].classList.add("destaque");
+        }
     }
-
 }
 
-function criaCalendarioIniciaSab(numeroDias) {
-    var diasVazio = 6;
+function criaCalendario(numeroDias, diasVazio) {
     for (let i = 1; i <= diasVazio; i++) {
         imprime(" ");
     }
 
+    dia = 1;
     for (let i = 1; i <= 42; i++) {
         while (dia <= numeroDias) {
-            mostraJan(dia);
+            if (mes == 1) {
+                mostraJan(dia);
+            } else if (mes == 2) {
+                mostraFev(dia);
+            } else if (mes == 3) {
+                mostraMar(dia);
+            } else if (mes == 4) {
+                mostraAbr(dia);
+            } else if (mes == 5) {
+                mostraMai(dia);
+            } else if (mes == 6) {
+                mostraJun(dia);
+            } else if (mes == 7) {
+                mostraJul(dia);
+            } else if (mes == 8) {
+                mostraAgo(dia);
+            } else if (mes == 9) {
+                mostraSet(dia);
+            } else if (mes == 10) {
+                mostraOut(dia);
+            } else if (mes == 11) {
+                mostraNov(dia);
+            } else if (mes == 12) {
+                mostraDez(dia);
+            }
             dia++;
         }
     }
@@ -155,7 +191,7 @@ function fdsDestaque(mes) {
         case 1:
             for (var i = 0; i < numeroDias; i++) {
                 var numDia = tdDias[i];
-                if ((i % 7 == 1) || (i % 7 == 2)) {
+                if ((i % 7 == 6) || (i % 7 == 0)) {
                     numDia.classList.add("destaque");
                 }
             }
@@ -164,7 +200,7 @@ function fdsDestaque(mes) {
         case 2:
             for (var i = 0; i < numeroDias; i++) {
                 var numDia = tdDias[i];
-                if ((i % 7 == 2) || (i % 7 == 3)) {
+                if ((i % 7 == 5) || (i % 7 == 6)) {
                     numDia.classList.add("destaque");
                 }
             }
@@ -182,7 +218,7 @@ function fdsDestaque(mes) {
         case 4:
             for (var i = 0; i < numeroDias; i++) {
                 var numDia = tdDias[i];
-                if ((i % 7 == 4) || (i % 7 == 5)) {
+                if ((i % 7 == 3) || (i % 7 == 4)) {
                     numDia.classList.add("destaque");
                 }
             }
@@ -191,7 +227,7 @@ function fdsDestaque(mes) {
         case 5:
             for (var i = 0; i < numeroDias; i++) {
                 var numDia = tdDias[i];
-                if ((i % 7 == 5) || (i % 7 == 6)) {
+                if ((i % 7 == 2) || (i % 7 == 3)) {
                     numDia.classList.add("destaque");
                 }
             }
@@ -200,11 +236,10 @@ function fdsDestaque(mes) {
         case 6:
             for (var i = 0; i < numeroDias; i++) {
                 var numDia = tdDias[i];
-                if ((i % 7 == 6) || (i % 7 == 0)) {
+                if ((i % 7 == 1) || (i % 7 == 2)) {
                     numDia.classList.add("destaque");
                 }
             }
-            
             break;
     }
 }
